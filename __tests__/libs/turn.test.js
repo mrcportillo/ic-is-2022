550a64feac29bacf5a01d40128b4e92b8aee9698
mrcportillo <mrcportillo@users.noreply.github.com>
2022-05-10 19:56:20 -0300

    added jest unit test configurations

---
+++ __tests__/libs/turn.test.js
@@ -0,0 +1,29 @@
+import { TURN_WAITING_TIME_BOUNDARIES } from "../../constants/turn";
+import { getTurnBorderColorByWaitingTime } from "../../libs/turn";
+
+describe("getTurnBorderColorByWaitingTime", () => {
+  describe("waiting time is below good waiting time boundary", () => {
+    test("color should be green", () => {
+      const color = getTurnBorderColorByWaitingTime(
+        TURN_WAITING_TIME_BOUNDARIES.GOOD - 1
+      );
+      expect(color).toBe("green");
+    });
+  });
+  describe("waiting time is above good waiting time boundary and below medium waiting time boundary", () => {
+    test("color should be gold", () => {
+      const color = getTurnBorderColorByWaitingTime(
+        TURN_WAITING_TIME_BOUNDARIES.MEDIUM - 1
+      );
+      expect(color).toBe("gold");
+    });
+  });
+  describe("waiting time is above medium waiting time boundary", () => {
+    test("color should be firebrick", () => {
+      const color = getTurnBorderColorByWaitingTime(
+        TURN_WAITING_TIME_BOUNDARIES.MEDIUM + 1
+      );
+      expect(color).toBe("firebrick");
+    });
+  });
+});
