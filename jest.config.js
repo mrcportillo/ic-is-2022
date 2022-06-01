f4c1dc4abd4acbc3a7b8753706b591f28fab9e2a
mrcportillo <mrcportillo@users.noreply.github.com>
2022-06-01 13:33:09 -0300

    added coverga report configuration

--- jest.config.js
+++ jest.config.js
@@ -12,6 +12,8 @@ const customJestConfig = {
   // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
   moduleDirectories: ["node_modules", "<rootDir>/"],
   testEnvironment: "jest-environment-jsdom",
+  coverageDirectory: "./coverage/",
+  collectCoverage: true,
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
